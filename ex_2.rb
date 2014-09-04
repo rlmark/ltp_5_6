16d771cfc7396e1264c3ad6eb0affa04a45c322d
Rebecca Mark <rebecca.lmark@gmail.com>
2014-09-03 22:50:05 -0700

    Exercise 2 first commit

---
+++ ex_2.rb
@@ -0,0 +1,9 @@
+# Bigger, better favorite number
+
+puts "Hey, what's your favorite number?"
+fav_num = gets.chomp
+
+bigger_num = fav_num.to_i + 1
+
+puts "Let's make your favorite number even more awesome!"
+puts "Here's your new number: #{bigger_num}."
