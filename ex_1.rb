e60f892f3971d2dfda465f155095294df30488df
Rebecca Mark <rebecca.lmark@gmail.com>
2014-09-03 22:49:43 -0700

    Exercise 1 first commit

---
+++ ex_1.rb
@@ -0,0 +1,12 @@
+# Full name greeting
+
+puts "Hi there, what's your first name?"
+first_name = gets.chomp
+
+puts "Okay, what's your middle name?"
+middle_name = gets.chomp
+
+puts "Alrighty, what is your last name?"
+last_name = gets.chomp
+
+puts "So your name is #{first_name + ' ' + middle_name + ' ' + last_name}, that's cool!"
